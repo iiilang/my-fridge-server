57120151fdca9c4f64ed9b4c00e189c3e10e35ae
minju <bbak0402dd@gmail.com>
2021-02-05 14:28:40 +0900

    Initial commit;)

---
+++ src/main/java/com/ilang/myfridge/MyFridgeApplication.java
@@ -0,0 +1,11 @@
+package com.ilang.myfridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFridgeApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(MyFridgeApplication.class, args);
+  }
+}
