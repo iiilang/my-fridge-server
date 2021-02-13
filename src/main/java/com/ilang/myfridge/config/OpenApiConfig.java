804617541aeecfc4ab8f8138ab450f1a7d614a07
minju <bbak0402dd@gmail.com>
2021-02-13 18:12:11 +0900

    feat: Add OpenAPI 3.0
    - API 명세를 위해 OpenAPI 3.0 붙임.

---
+++ src/main/java/com/ilang/myfridge/config/OpenApiConfig.java
@@ -0,0 +1,30 @@
+package com.ilang.myfridge.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(info = @Info(title = "마이냉장고 API 명세서", description = "API 명세서", version = "0.1"))
+@Configuration
+public class OpenApiConfig {
+  @Bean
+  public GroupedOpenApi fridgeApis() {
+    return GroupedOpenApi.builder().setGroup("fridge").pathsToMatch("/**/fridge/**").build();
+  }
+
+  @Bean
+  public GroupedOpenApi foodApis() {
+    return GroupedOpenApi.builder().setGroup("food").pathsToMatch("/**/food/**").build();
+  }
+
+  @Bean
+  public GroupedOpenApi userApis() {
+    return GroupedOpenApi.builder().setGroup("user").pathsToMatch("/**/user/**").build();
+  }
+}
