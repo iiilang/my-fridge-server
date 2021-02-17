67c52418b9c0dda551fb2a80ae315940c1b77297
minju <bbak0402dd@gmail.com>
2021-02-17 20:13:36 +0900

    feat: Add BaseTimeEntity and Test API ping()
    - BaseTimeEntity 추가, 디렉터리 구조 변경, 배포 테스트 위한 임시 API 생성

---
+++ src/main/java/com/ilang/myfridge/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.ilang.myfridge.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+  @GetMapping("/ping")
+  public String ping() {
+    return "pong";
+  }
+}
---
+++ src/main/java/com/ilang/myfridge/model/BaseTimeEntity.java
@@ -0,0 +1,20 @@
+package com.ilang.myfridge.model;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+  @CreatedDate private LocalDateTime createdDate;
+
+  @LastModifiedDate private LocalDateTime modifiedDate;
+}
